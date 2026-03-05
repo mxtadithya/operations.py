4420202ae0c8c10fc7da69929b24e980b7fba622
mxtadithya <adiadithyaajayan@gmail.com>
2026-03-05 09:17:20 +0530

    operations

---
+++ helo.py
@@ -0,0 +1,17 @@
+print('halooo')
+#name='adhi'
+#age=21
+#course='python'
+#is_adult= True
+#print (age)
+
+#MULTIPLE VARIASBLE ASSINGMENT
+name,age,course='adhi',21,'python'
+print(course)
+print('name:',name)
+
+# assigning same value to different variables
+x=y=z= 10
+print('x:',x)
+print('y:',x)
+print('z:',x)
---
+++ operations.py
@@ -0,0 +1,28 @@
+#arithematic operations
+a=10
+b=5
+p=10
+q=3
+name='adithya'
+print(a+b)#addition
+print(a-b)#subtraction
+print(a*b)#multiplication
+print(a/b)#division
+print(p/q)#division
+print(p//q)#floor division .values ozhivakkaan
+print(p%q)#exponential 10*10*10
+
+print(name*2)
+
+
+#assignment operators
+x=60
+x+=5 #x=x+5
+x-+5 #x=x-5
+x*=5 #x=x*5
+x/=5 #x=x/5
+x//=5 #x=x//5
+x**=5
+print('x:',x)
+
+
