3010eefc03bf22a3a86f30ccb3601139ecdd4b0a
mxtadithya <adiadithyaajayan@gmail.com>
2026-03-09 08:54:38 +0530

    conditions added

---
+++ conditions.py
@@ -0,0 +1,59 @@
+#age=32 
+# if age>=18:
+    # print("eligeble for vote")
+# else:
+    # print("not eligeble")
+
+##if elif else
+# mark=87
+# if mark>=90:
+ #    print("grade A")
+# elif mark>=80:
+   # print("grade B")
+#else: ("fail")
+
+
+#####nested conditiond=s
+
+# has_id=True
+#if age>=20:
+ #   if has_id:
+ #           print("ytou are eligble")
+   # else:   
+    #        print("must you have id")
+#else:
+ #     print("not eligebe")
+
+
+#age=22
+
+#has_id =False
+
+#gender ="male"
+
+#if# age >18 and has_id:
+ #   print("you are eligible")
+#else:
+ #   print("both conditions are not valid")
+
+#if age>18 or gender=="male":
+ #   print("you are eligible")
+#print(has_id)
+
+#if not has_id:
+   # print("your id is not valid")
+
+############################################
+#task
+
+num=120
+if num>0:
+    print("the number is positve")
+else:
+    print("the number is negative")
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+if num > 100:
+    print("The number is greater than 100")
