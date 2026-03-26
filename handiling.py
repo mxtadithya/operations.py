4c9488b7d26127d7d20f7ee7c0c752a98394aad0
mxtadithya <adiadithyaajayan@gmail.com>
2026-03-26 09:14:08 +0530

    oops

--- handiling.py
+++ handiling.py
@@ -18,20 +18,20 @@ file=open("examplehandiling.txt","w")
 #         print(file.read())
 
 
-f = open('students.txt','w')
-for i in range(1,11):
-    f.write('Hello'+'\n')
+# f = open('students.txt','w')
+# for i in range(1,11):
+#     f.write('Hello'+'\n')
     
-f = open('students.txt','r')
+# f = open('students.txt','r')
 
-data = f.read(3)
-print(f.tell())
-f.seek(0)
-print(f.tell())
-data1 = f.read()
+# data = f.read(3)
+# print(f.tell())
+# f.seek(0)
+# print(f.tell())
+# data1 = f.read()
 
-print(data)
-print(data1)
+# print(data)
+# print(data1)
 
 # first_line = f.readline(2)
 # remaining = f.readline()
@@ -41,3 +41,37 @@ print(data1)
 # data = f.readlines()
 # for i in data:
 #     print(i)
+
+#module access cheyyunnath
+# import functions
+# from functions import is_even
+# print(is_even(8),'file handling')
+# print(functions.is_even(10))
+
+
+import os
+
+# os.remove('sample.txt')
+
+# if os.path.exists('sample.txt'):
+#     os.remove('sample.txt')
+    
+# os.rmdir('folders')
+
+import math
+# print(math.floor(8.12))
+# print(math.sqrt(8.12))
+# print(math.ceil(8.12))
+# print(math.pi)
+import random
+from datetime import datetime,date
+print(math.floor(random.random()*1e6))
+print(random.randint(1,9))
+print(random.choice(['blue','black','green']))
+now = datetime.now()
+print(now)
+print(now.date())
+print(now.month)
+print(now.day)
+print(now.year)
+print(date.today())
--- oop.py
+++ oop.py
@@ -1,9 +1,36 @@
-class syneffo:
-    def python():
-        print('python')
-    def javascript():
-        print('javascript') 
+# class syneffo:
+#     def python():
+#         print('python')
+#     def javascript():
+#         print('javascript') 
 
-std1 = syneffo
-std1.python()
-std1.javascript()
+# std1 = syneffo
+# std1.python()
+# std1.javascript()
+
+
+class student:
+    school='ABC public school'  #class variables
+    def __init__(self,name,age): #parameterized constructor
+        self.name=name #instance variables
+        self.age=age
+    def getDetails(self):
+        print(f"Name is{self.name}.\nAge is {self.age}.")
+    @staticmethod
+    def great():
+        print('hello')
+        
+    @classmethod
+    def change_school(cls,name):
+        cls.school=name
+p1 = student('mishal',23)
+
+print(p1.name)
+print(p1.age)
+# print(p1.phone)
+p1.getDetails()
+student.great()
+
+print(student.school)
+student.change_school('carmal rani public school')
+print(student.school)
